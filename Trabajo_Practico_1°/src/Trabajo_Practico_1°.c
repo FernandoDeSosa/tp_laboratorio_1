adb66ace224d072a4535a76bcc4d8f1eb61b6598
FernandoDeSosa <68007184+FernandoDeSosa@users.noreply.github.com>
2021-04-15 08:59:14 -0300

    Add files via upload

---
+++ Trabajo_Practico_1°/src/Trabajo_Practico_1°.c
@@ -0,0 +1,188 @@
+/*
+ ============================================================================
+ Name        : Trabajo_Practico_1°.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float PedirNumero(char* mensaje);
+float Suma(float x, float y);
+float Resta(float x, float y);
+float Division(float x, float y);
+float Multiplicacion(float x, float y);
+float Factorial(float numero);
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+
+	int opcion;
+	float a;
+	float b;
+	float suma;
+	float resta;
+	float division;
+	float multiplicacion;
+	float factorialA;
+	float factorialB;
+	int bandera1;
+	int bandera2;
+
+
+	bandera1 = 0;
+    bandera2 = 0;
+	do
+	{
+	    printf("***********Bienvedio a la calculadora************\n");
+		printf("\n 1. Ingrese 1er operando A = %.2f . \n 2. Ingrese 2do operando B = %.2f . \n 3. Calcular todas las operaciones. \n 4. Informar resultados. \n 5. Salir. \n",a,b);
+		printf("\n *************************************************\n");
+		scanf("%d", &opcion);
+		system("CLS");
+
+		switch(opcion)
+		{
+		case 1:
+			a = PedirNumero("Ingrese 1er numero: ");
+			bandera1 = 1;
+
+			break;
+		case 2:
+			b = PedirNumero("Ingrese 2do numero: ");
+			bandera2 = 1;
+
+			break;
+		case 3:
+		    if(bandera1 == 0 || bandera2 == 0)
+            {
+                printf("Error, falta ingresar operando.\n");
+            }
+            else
+            {
+                suma = Suma(a,b);
+                resta = Resta(a,b);
+                division = Division(a,b);
+                multiplicacion = Multiplicacion (a,b);
+                factorialA = Factorial(a);
+                factorialB = Factorial(b);
+            }
+
+
+
+			break;
+		case 4:
+		    if(bandera1 == 0 || bandera2 == 0)
+            {
+                printf("Error, falta ingresar operando.\n");
+            }
+            else
+            {
+
+                printf("El resultado de %.2f + %.2f es: %.2f \n",a,b,suma);
+                printf("El resultado de %.2f - %.2f es: %.2f \n",a,b,resta);
+
+                if(b == 0)
+                {
+                    printf("No es posible dividir por 0. \n");
+                }
+                else
+                {
+                    printf("El resultado de %.2f / %.2f es: %.2f \n",a,b,division);
+                }
+
+                printf("El resultado de %.2f * %.2f es: %.2f \n",a,b,multiplicacion);
+
+                if(factorialA != 0)
+                {
+                    printf("El factorial de %.2f es: %.2f \n", a,factorialA);
+                }
+                else
+                {
+                    printf("El factorial de 0 es 1.\n");
+                }
+
+                if(factorialB != 0)
+                {
+                    printf("El factorial de %.2f es: %.2f \n", b,factorialB);
+                }
+                else
+                {
+                    printf("El factorial de 0 es 1.\n");
+                }
+                }
+                break;
+
+		}
+
+	}while(opcion != 5);
+
+
+
+	return EXIT_SUCCESS;
+}
+
+float PedirNumero(char* mensaje)
+{
+	float numeroRegresado;
+
+	printf("%s", mensaje);
+	scanf("%f", &numeroRegresado);
+
+	return numeroRegresado;
+}
+
+float Suma(float x, float y)
+{
+	float sumaRegresada;
+
+	sumaRegresada = x + y;
+
+	return sumaRegresada;
+}
+
+float Resta(float x, float y)
+{
+	float restaRegresada;
+
+	restaRegresada = x - y;
+
+	return restaRegresada;
+}
+
+float Division(float x, float y)
+{
+	float divisionRetornada;
+
+	divisionRetornada = x / y;
+
+	return divisionRetornada;
+}
+
+float Multiplicacion(float x, float y)
+{
+	float multiplicacionRetornada;
+
+	multiplicacionRetornada = x * y;
+
+	return multiplicacionRetornada;
+}
+
+float Factorial(float numero)
+{
+	int i;
+	int factorial;
+
+	factorial = 1;
+
+	for(i = 1; i < numero; i++)
+	{
+		factorial = factorial * i;
+	}
+
+	return factorial;
+}
